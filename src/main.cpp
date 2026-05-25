462abfb043baa373a706f221dfea29be9cf15d29
RAFAEL FARIAS DE ARAUJO <58945331808@info.local>
2026-05-25 17:17:00 -0300

    Implementada as funções de direcionas, power , up e down.

--- src/main.cpp
+++ src/main.cpp
@@ -1,104 +1,118 @@
-/* 
-========================================
-CONTROLE IR TV LG COM ESP32-S3
-========================================
-
-Funcoes:
-- Liga/Desliga TV
-- Volume +
-- Volume -
-
-Comandos enviados pelo Monitor Serial
-
-Digite:
-P -> POWER
-+ -> VOLUME +
-- -> VOLUME -
-
-========================================
-*/
 
 #include <Arduino.h>
 #include <IRremoteESP8266.h>
 #include <IRsend.h>
+#include <ArduinoJson.h>
+//-----------------
+#include "WiFiManager.h"
+#include "MQTTmanager.h"
+#include "DebugManager.h"
+#include "secrets.h"
 
-// ======================================
-//PINO DO LED INFRAVERMELHO
-// ======================================
+const uint16_t pinIR = 4; //?Pino onde o LED IR está conectado. 
 
-const uint16_t pinIR = 4;
-
-// ======================================
-// CODIGOS IR DA TV LG
-// (podem mudar dependendo da TV)
-// ======================================
-
+ 
 const uint32_t codigoPower = 0x20DF10EF;
 const uint32_t codigoVolMais = 0x20DF40BF;
 const uint32_t codigoVolMenos = 0x20DFC03F;
+const uint32_t codigoSetaDireita = 0x20DF609F;
+const uint32_t codigoSetaEsquerda = 0x20DFE01F
+const uint32_t codigoSetaCima = 0x20DF02FD;
+const uint32_t codigoSetaBaixo = 0x20DF827D;
+const uint32_t codigoOK = 0x20DF22DD;
 
-// ======================================
-// OBJETO RESPONSAVEL PELO IR
-// ======================================
+const char TOPICO_COMANDO[] = "senai134/dev/yoshi/teste";
 
-IRsend irsend(pinIR);
+IRsend irsend(pinIR); //?Cria um objeto IRsend para controlar o LED IR conectado ao pino especificado.
+
+
+
+void tratarMensagemRecebida(const char *topico, const String &mensagem);
+void tratarJsonComando(const String &mensagem);
+void PowerTV();
+void VolumeMais();
+void VolumeMenos();
+
 
 void setup()
 {
     // Inicia serial
     Serial.begin(115200);
-
-    // Inicializa transmissor IR
     irsend.begin();
 
-    Serial.println("================================");
-    Serial.println("CONTROLE TV LG");
-    Serial.println("================================");
-    Serial.println("P -> POWER");
-    Serial.println("+ -> VOLUME +");
-    Serial.println("- -> VOLUME -");
-    Serial.println("================================");
+    configurarDebug();  //?Configura o sistema de debug (geralmente para monitorar o status do dispositivo e mensagens de erro).
+
+    conectarWifi();
+    conectarMQTT();
+    registrarCallbackMensagem(tratarMensagemRecebida);
+    conectarMQTT();
 }
 
 void loop()
 {
-    // Verifica se algo foi digitado
-    if (Serial.available())
-    {
-        // Le caractere digitado
-        char comando = Serial.read();
+    garantirWifiConectado();
+    garantirMQTTConectado();
+    loopMQTT();
+}   
 
-        // ==========================
-        // POWER
-        // ==========================
+// TODO - Adicionar DebugInfo e DebugErro para monitorar o status do dispositivo e mensagens de erro.
+// TODO - Criar função para tratar o JSON recebido via MQTT e executar as ações correspondentes (Ligar/Desligar TV, Aumentar/Diminuir Volume).
 
-        if (comando == 'P' || comando == 'p')
-        {
-            irsend.sendLG(codigoPower, 32);
 
-            Serial.println("TV Ligada/Desligada");
-        }
 
-        // ==========================
-        // VOLUME +
-        // ==========================
 
-        else if (comando == '+')
-        {
-            irsend.sendLG(codigoVolMais, 32);
 
-            Serial.println("Volume Aumentado");
-        }
+void PowerTV()
+{
+  irsend.sendLG(codigoPower, 32);
+  debugInfo("TV Ligada/Desligada"); 
+}
 
-        // ==========================
-        // VOLUME -
-        // ==========================
+void VolumeMais()
+{
+  irsend.sendLG(codigoVolMais, 32);
+  debugInfo("Volume Aumentado");
+}
 
-        else if (comando == '-')
-        {
-            irsend.sendLG(codigoVolMenos, 32);
+void VolumeMenos()
+{
+  irsend.sendLG(codigoVolMenos, 32);
+  debugInfo("Volume Diminuido");
+}
 
-            Serial.println("Volume Diminuido");
-        }
-    }
+void ShareTela()
+{
+  // TODO - Implementar macro de sequencia para compartilhar tela
+
+
+}
+
+void SetaDireita()
+{
+  irsend.sendLG(codigoSetaDireita, 32);
+  debugInfo("Seta Direita");
+}
+
+void SetaEsquerda()
+{
+  irsend.sendLG(codigoSetaEsquerda, 32);
+  debugInfo("Seta Esquerda");
+}
+
+void SetaCima()
+{
+  irsend.sendLG(codigoSetaCima, 32);
+  debugInfo("Seta Cima");
+}
+
+void SetaBaixo()
+{
+  irsend.sendLG(codigoSetaBaixo, 32);
+  debugInfo("Seta Baixo");
+}   
+
+void OK()
+{
+  irsend.sendLG(codigoOK, 32);
+  debugInfo("OK");
 }
